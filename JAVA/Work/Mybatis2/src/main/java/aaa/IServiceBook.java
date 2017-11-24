09eb88d2867562b0ab5002b0395390cdfd431daf
gunee7 <gunee7@naver.com>
2017-11-24 10:41:42 +0900

    qwe

---
+++ JAVA/Work/Mybatis2/src/main/java/aaa/IServiceBook.java
@@ -0,0 +1,8 @@
+package aaa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface IServiceBook extends IBook {
+    
+}
---
+++ JAVA/Work/Mybatis2/src/main/java/aaa/ModelAuth.java
@@ -0,0 +1,9 @@
+package aaa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModelAuth {
+    // SLF4J Logging
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+}
