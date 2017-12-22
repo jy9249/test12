ef352d05b33a8bfb8272a50c26e0eb550e6f0f6b
gunee7 <gunee7@naver.com>
2017-12-22 16:03:01 +0900

    dfs

--- Android/Work/Dialog/app/src/main/java/com/example/administrator/dialog/MainActivity.java
+++ Android/Work/Dialog/app/src/main/java/com/example/administrator/dialog/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.administrator.dialog;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,20 +8,38 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn, btn2;
+    AlertDialog.Builder dialog;
+    final String[] versionArray = new String[]{"젤리빈", "킷켓", "롤리팝"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder dlg = new AlertDialog.Builder(AlertDialog.this);
-                dlg.setTitle("제목입니다");
-                dlg.setMessage("이곳이 내용입니다.");
-                dlg.setIcon(R.drawable.ic_launcher_background);
-                dlg.show();
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("제목입니다")
+                        .setMessage("이곳이 내용입니다.")
+                        .setIcon(R.drawable.movie_icon)
+                        .show();
 
+            }
+        });
+
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("제목입니다");
+                dialog.setMessage("이곳이 내용입니다.");
+                dialog.setIcon(R.drawable.movie_icon);
+                dialog.setPositiveButton("확인", null);
+                dialog.show();
             }
         });
 
